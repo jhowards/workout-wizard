6e3f70af81c2d4bf102640a3b0fa187107ccb769
jhowards <github@jameshoward.uk>
2021-09-30 17:28:29 +0100

    Home layout + sidebar css

---
+++ src/components/Home.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import SideBar from "./SideBar";
+import "../css/Home.css";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import profilephoto from "../images/placeholder.png";
+
+const Home = () => {
+  return (
+    <div className="d-flex h-100">
+      <SideBar />
+      <div className="h-100 w-100 home_mainbody py-lg-3 px-lg-5">
+        <Row xs={1} md={2} className="g-2 h-100">
+          <Col className="px-4 d-flex flex-column h-100">
+            <Card
+              className="home_dailyheader mb-3 mt-4"
+              style={{ width: "100%" }}
+            >
+              <Card.Body>
+                <Row>
+                  <Col xs={12} lg={4} className="px-3">
+                    <Card.Img
+                      className="home_dailyheader_image"
+                      src={profilephoto}
+                    />
+                  </Col>
+                  <Col xs={12} lg={8} className="text-center">
+                    <Card.Title className="home_dailyheader_morningtext mt-5">
+                      Good Morning James
+                    </Card.Title>
+                    <Card.Text className="home_dailyheader_taskstext">
+                      Tasks Today: 7
+                    </Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+            <div className="home_timeperiod mb-2 ml-4">
+              <span className="mr-2 home_timeperiod_text_active">Daily</span>
+              <span className="mr-2 home_timeperiod_text">Weekly</span>
+              <span className="home_timeperiod_text">Monthly</span>
+            </div>
+            <Card className="h-100 home_schedule" style={{ width: "100%" }}>
+              <Card.Body className="h-100">
+                <Card.Title className="home_schedule_todaytext">
+                  Today's Tasks
+                </Card.Title>
+                <Card.Text className="home_schedule_datetext">
+                  30th October 2021
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Button className="home_schedule_button mx-auto mt-3 mb-4">
+              View Schedule
+            </Button>
+          </Col>
+          <Col className="px-4 d-flex flex-column h-100">
+            <Card
+              className="mt-4 text-center home_calendar"
+              style={{ width: "100%", height: "50%" }}
+            >
+              <Card.Body>
+                <Card.Title>Calendar</Card.Title>
+              </Card.Body>
+            </Card>
+            <Button className="home_schedule_button mx-auto mt-3 mb-4">
+              View Calendar
+            </Button>
+            <h4>Goals</h4>
+            <div className="d-flex flex-column">
+              <Card
+                className="mt-0 home_goals"
+                style={{ width: "100%", height: "90px" }}
+              >
+                <Card.Body className="home_goals_goalbody d-flex">
+                  <div className="home_goals_cardcolor_1 my-auto mr-3"></div>
+                  <div>
+                    <Card.Text className="home_goals_goaltag_1 mb-1">
+                      Reading
+                    </Card.Text>
+                    <Card.Title className="home_goals_goaltext mb-2">
+                      Finish Self Help Book
+                    </Card.Title>
+                    <Card.Text className="home_goals_goaldeadline">
+                      November Deadline
+                    </Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+              <Card
+                className="mt-2 home_goals"
+                style={{ width: "100%", height: "90px" }}
+              >
+                <Card.Body className="home_goals_goalbody d-flex">
+                  <div className="home_goals_cardcolor_2 my-auto mr-3"></div>
+                  <div>
+                    <Card.Text className="home_goals_goaltag_2 mb-1"></Card.Text>
+                    <Card.Title className="home_goals_goaltext mb-2"></Card.Title>
+                    <Card.Text className="home_goals_goaldeadline"></Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+              <Card
+                className="mt-2 home_goals"
+                style={{ width: "100%", height: "90px" }}
+              >
+                <Card.Body className="home_goals_goalbody d-flex">
+                  <div className="home_goals_cardcolor_3 my-auto mr-3"></div>
+                  <div>
+                    <Card.Text className="home_goals_goaltag_3 mb-1"></Card.Text>
+                    <Card.Title className="home_goals_goaltext mb-2"></Card.Title>
+                    <Card.Text className="home_goals_goaldeadline"></Card.Text>
+                  </div>
+                </Card.Body>
+              </Card>
+              <Button className="home_schedule_button mx-auto mt-3 mb-4">
+                View Goals
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
