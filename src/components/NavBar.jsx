fc7ab995d1a3c8c7a8a6bdb306ed4fda5acaecd2
jhowards <github@jameshoward.uk>
2021-09-30 17:49:43 +0100

    Navbar Responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <Navbar variant="dark" className="login_navbar" expand="lg">
-      <Navbar.Brand className="ml-5" href="#">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand className="mr-0" href="#">
         Task Manager
       </Navbar.Brand>
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto mt-2">
+          <Nav.Link className="d-block d-md-none" href="#home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="d-block d-md-none" href="#">
+            Schedule
+          </Nav.Link>
+          <Nav.Link className="d-block d-md-none" href="#">
+            Calendar
+          </Nav.Link>
+          <Nav.Link className="d-block d-md-none" href="#">
+            Dailies
+          </Nav.Link>
+          <Nav.Link className="d-block d-md-none" href="#">
+            Notes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
