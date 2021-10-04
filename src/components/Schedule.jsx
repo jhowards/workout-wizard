93a99667b98ca7f40a5ce56afe73bfb9de1d2b21
jhowards <github@jameshoward.uk>
2021-10-04 17:44:31 +0100

    Tasks component + redux

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import SideBar from "./SideBar";
 import "../css/Schedule.css";
 import { Container, Button, Row, Col, Form } from "react-bootstrap";
-import { BsThreeDotsVertical } from "react-icons/bs";
+
 import { FaPlus } from "react-icons/fa";
+import { Tasks } from "./Tasks";
 
 const Schedule = () => {
   return (
     <div className="d-flex h-100">
       <SideBar />
-      <div className="w-100 schedule_mainbody py-lg-3 px-lg-5">
+      <div className="h-100 w-100 schedule_mainbody py-lg-3 px-lg-5">
         <Container className="schedule_container_large">
           <div className="schedule_heading text-center mt-3 mb-2">
             <p className="schedule_weekday mb-0">Wednesday</p>
@@ -70,96 +71,8 @@ const Schedule = () => {
                 </Button>
               </div>
             </div>
-            <div className="schedule_activeschedule_body mt-2">
-              <Row className="mx-0">
-                <Col
-                  xs={2}
-                  className="schedule_activeschedule_body_times mt-3 pl-5 pr-0"
-                >
-                  <div className="d-flex flex-column schedule_activeschedule_body_times_margins">
-                    <div className="mb-5">
-                      <p className="mb-0 schedule_activeschedule_body_times_largetime">
-                        6:30am
-                      </p>
-                      <p className="schedule_activeschedule_body_times_smalltime">
-                        7:00am
-                      </p>
-                    </div>
-                    <div className="mb-5">
-                      <p className="mb-0 schedule_activeschedule_body_times_largetime">
-                        7:00am
-                      </p>
-                      <p className="schedule_activeschedule_body_times_smalltime">
-                        7:40am
-                      </p>
-                    </div>
-                  </div>
-                </Col>
 
-                <Col
-                  xs={10}
-                  className="schedule_activeschedule_body_tasks d-flex flex-row px-0"
-                >
-                  <div className="schedule_activeschedule_body_divider ml-5 mr-5"></div>
-                  <div className="d-flex flex-column w-100 mr-5">
-                    <div className="schedule_activeschedule_body_taskcontent">
-                      <Row className="mx-0 h-100 position-relative">
-                        <Col className="px-4 d-flex" xs={1}>
-                          <Form.Check
-                            custom
-                            type="checkbox"
-                            id="test"
-                            className="m-auto"
-                          />
-                        </Col>
-                        <Col className="px-4 mt-2" xs={11}>
-                          <s className="schedule_activeschedule_body_taskcontent_tasktext mb-0 d-block">
-                            Wake up and get ready
-                          </s>
-                          <s className="schedule_activeschedule_body_taskcontent_tasktimetext mt-0 mb-0 d-block">
-                            {"30m -> 7:00am"}
-                          </s>
-                          <s className="schedule_activeschedule_body_taskcontent_dailybadge text-center mt-1 d-block">
-                            Daily
-                          </s>
-                          <BsThreeDotsVertical
-                            size={26}
-                            className="schedule_activeschedule_body_taskcontent_edittask"
-                          />
-                        </Col>
-                      </Row>
-                    </div>
-                    <div className="schedule_activeschedule_body_taskcontent_active mt-3">
-                      <Row className="mx-0 h-100">
-                        <Col className="position-relative" xs={2}>
-                          <Form.Check
-                            custom
-                            type="checkbox"
-                            id="test2"
-                            className="testtt"
-                          />
-                        </Col>
-                        <Col className="px-0 mt-2" xs={10}>
-                          <p className="schedule_activeschedule_body_taskcontent_tasktext mb-0">
-                            Morning Exercise{" "}
-                          </p>
-                          <p className="schedule_activeschedule_body_taskcontent_tasktimetext mt-0 mb-0">
-                            {"40m -> 7:40am"}
-                          </p>
-                          <div className="schedule_activeschedule_body_taskcontent_dailybadge text-center mt-1">
-                            Daily
-                          </div>
-                          <BsThreeDotsVertical
-                            size={26}
-                            className="schedule_activeschedule_body_taskcontent_edittask"
-                          />
-                        </Col>
-                      </Row>
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-            </div>
+            <Tasks />
           </div>
         </Container>
       </div>
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,207 @@
+import React from "react";
+import { Row, Col, Form } from "react-bootstrap";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
+export const Tasks = () => {
+  const tasks = [
+    {
+      id: 1,
+      archived: true,
+      daily: true,
+      task: "Wake up and get ready",
+      date: "30m -> 7:00am",
+      starttime: "6:30am",
+      endtime: "7:00am",
+      active: false,
+    },
+    {
+      id: 2,
+      archived: false,
+      daily: true,
+      task: "Morning Exercise",
+      date: "40m -> 7:40am",
+      starttime: "7:00am",
+      endtime: "7:40am",
+      active: true,
+    },
+    {
+      id: 3,
+      archived: false,
+      daily: true,
+      task: "Eat Breakfast",
+      date: "30m -> 8:30am",
+      starttime: "8:00am",
+      endtime: "8:30am",
+      active: false,
+    },
+    {
+      id: 4,
+      archived: false,
+      daily: false,
+      task: "Study for Project",
+      date: "2hr30m -> 12:00pm",
+      starttime: "8:30am",
+      endtime: "12:00pm",
+      active: false,
+    },
+  ];
+
+  return (
+    <div className="schedule_activeschedule_body mt-2" data-testid="tasks">
+      {tasks.map((task) =>
+        task.archived === true ? (
+          <Row className="mx-0" key={`${task.id}`}>
+            <Col
+              xs={2}
+              className="schedule_activeschedule_body_times mt-3 pl-5 pr-0"
+            >
+              <div className="d-flex flex-column schedule_activeschedule_body_times_margins">
+                <div>
+                  <p className="mb-0 schedule_activeschedule_body_times_largetime">
+                    {task.starttime}
+                  </p>
+                  <p className="schedule_activeschedule_body_times_smalltime">
+                    {task.endtime}
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col
+              xs={10}
+              className="schedule_activeschedule_body_tasks d-flex flex-row px-0"
+            >
+              <div className="schedule_activeschedule_body_divider ml-5 mr-5"></div>
+              <div className="d-flex flex-column w-100 mr-5">
+                <div className="schedule_activeschedule_body_taskcontent mb-3">
+                  <Row className="mx-0 h-100">
+                    <Col className="position-relative" xs={2}>
+                      <Form.Check
+                        custom
+                        type="checkbox"
+                        id={task.id}
+                        className="testtt"
+                      />
+                    </Col>
+                    <Col className="px-0 mt-2" xs={10}>
+                      <s className="schedule_activeschedule_body_taskcontent_tasktext mb-0 d-block">
+                        {task.task}
+                      </s>
+                      <s className="schedule_activeschedule_body_taskcontent_tasktimetext mt-0 mb-0 d-block">
+                        {task.date}
+                      </s>
+                      <s className="schedule_activeschedule_body_taskcontent_dailybadge text-center mt-1 d-block">
+                        Daily
+                      </s>
+                      <BsThreeDotsVertical
+                        size={26}
+                        className="schedule_activeschedule_body_taskcontent_edittask"
+                      />
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        ) : (
+          <Row className="mx-0" key={`${task.id}`}>
+            <Col
+              xs={2}
+              className="schedule_activeschedule_body_times mt-3 pl-5 pr-0"
+            >
+              <div className="schedule_activeschedule_body_times_margins">
+                <div>
+                  <p className="mb-0 schedule_activeschedule_body_times_largetime">
+                    {task.starttime}
+                  </p>
+                  <p className="schedule_activeschedule_body_times_smalltime">
+                    {task.endtime}
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col
+              xs={10}
+              className="schedule_activeschedule_body_tasks d-flex flex-row px-0"
+            >
+              <div className="schedule_activeschedule_body_divider ml-5 mr-5"></div>
+              <div className="d-flex flex-column w-100 mr-5">
+                {task.active ? (
+                  <div className="schedule_activeschedule_body_taskcontent_active mb-3">
+                    <Row className="mx-0 h-100">
+                      <Col className="position-relative" xs={2}>
+                        <Form.Check
+                          custom
+                          type="checkbox"
+                          id={task.id}
+                          className="testtt"
+                        />
+                      </Col>
+                      <Col className="px-0 mt-2" xs={10}>
+                        <p className="schedule_activeschedule_body_taskcontent_tasktext mb-0">
+                          {task.task}
+                        </p>
+                        <p className="schedule_activeschedule_body_taskcontent_tasktimetext mt-0 mb-0">
+                          {task.date}
+                        </p>
+                        <p className="schedule_activeschedule_body_taskcontent_dailybadge text-center mt-1">
+                          Daily
+                        </p>
+                        <BsThreeDotsVertical
+                          size={26}
+                          className="schedule_activeschedule_body_taskcontent_edittask"
+                        />
+                      </Col>
+                    </Row>
+                  </div>
+                ) : (
+                  <div className="schedule_activeschedule_body_taskcontent mb-3">
+                    <Row className="mx-0 h-100">
+                      <Col className="position-relative" xs={2}>
+                        <Form.Check
+                          custom
+                          type="checkbox"
+                          id={task.id}
+                          className="testtt"
+                        />
+                      </Col>
+                      <Col className="px-0 mt-2" xs={10}>
+                        <p className="schedule_activeschedule_body_taskcontent_tasktext mb-0">
+                          {task.task}
+                        </p>
+                        {task.daily ? (
+                          <>
+                            <p className="schedule_activeschedule_body_taskcontent_tasktimetext mt-0 mb-0">
+                              {task.date}
+                            </p>
+                            <p className="schedule_activeschedule_body_taskcontent_dailybadge text-center mt-1">
+                              Daily
+                            </p>
+                          </>
+                        ) : (
+                          <>
+                            <p className="schedule_activeschedule_body_taskcontent_tasktimenodaily mt-2 mb-0">
+                              {task.date}
+                            </p>
+                          </>
+                        )}
+
+                        <BsThreeDotsVertical
+                          size={26}
+                          className="schedule_activeschedule_body_taskcontent_edittask"
+                        />
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+              </div>
+            </Col>
+          </Row>
+        )
+      )}
+    </div>
+  );
+};
+
+{
+  /* <Checkbox id={task.id} taskDesc={task.task} /> */
+}
